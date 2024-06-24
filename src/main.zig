c27acfc9ecad51120737c42263ed17b49a0d5123
sirlilpanda <sirlilpanda@gmail.com>
2024-06-24 18:07:54 +1200

    updated to reflect changes made in other files

--- src/main.zig
+++ src/main.zig
@@ -12,121 +12,100 @@ const program = @import("opengl_wrappers/program.zig");
 const render = @import("opengl_wrappers/render.zig");
 const uni = @import("opengl_wrappers/uniform.zig");
 const cam = @import("opengl_wrappers/camera.zig");
+const window = @import("opengl_wrappers/window.zig");
+const tex = @import("opengl_wrappers/texture.zig");
+const cprint = @import("console_logger/coloured_text.zig");
 const obj = @import("objects/object.zig");
-
+const file = @import("file_loading/loadfile.zig");
 const basic = @import("basic.zig");
+const obj_loader = @import("objects/object_loader_service.zig");
 
 var angle: f32 = 0;
 const CDR: f32 = std.math.pi / 180.0;
 
-var seaShells: [32]obj.Object = undefined;
+var obj_loader_service: obj_loader.ObjectService = undefined;
+var index: u32 = 0;
+var objects: [512]obj.Object = undefined;
+var crab: obj.Object = undefined;
 
 var camera: cam.Camera = undefined;
-
-// var viewMatrix: mat.Mat4x4 = undefined;
-// var projMatrix: mat.Mat4x4 = undefined;
-var num_triangles: usize = undefined;
 var keep_running = false;
 
-var prog: basic.BasicProgram = undefined;
+var prog: basic.BasicProgramTex = undefined;
+var printer = cprint.ColourPrinter.init();
 
 const screen_hight = 1080;
 const screen_width = 1920;
-
-fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
-    _ = p;
-    return glfw.getProcAddress(proc);
-}
-
-/// Default GLFW error handling callback
-fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
-    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
-}
 
 const win_size = 256;
 var win: @Vector(win_size, f32) = @splat(0);
 var i: u8 = 0;
 
 fn spin() void {
+    printer.clear();
     while (!keep_running) {
-        std.debug.print("fps : {d:.2}, angle : {d:.2}, pos : x:{d:.3}, y:{d:.3}, z:{d:.3}, lookat : x:{d:.3}, y:{d:.3}, z:{d:.3}\n", .{
-            @reduce(.Add, win),
-            angle,
-            camera.eye.vec[0],
-            camera.eye.vec[1],
-            camera.eye.vec[2],
-            camera.look_at_point.vec[0],
-            camera.look_at_point.vec[1],
-            camera.look_at_point.vec[2],
-        });
         angle += 0.5;
         // std.debug.print("angle : {}\n", .{angle});
+        if (angle > 360) angle = 0;
+        printDebug();
         time.sleep(1000000);
     }
 }
 
+fn printDebug() void {
+    printer.setFgColour(cprint.Colour.purple());
+    const fps = @reduce(.Add, win);
+    std.debug.print("fps:", .{});
+    printer.print("{}\n", .{fps});
+
+    std.debug.print("angle:", .{});
+    printer.print("{}\n", .{angle});
+
+    std.debug.print("pos:", .{});
+    printer.vec3(camera.eye);
+    std.debug.print("\nlookat:", .{});
+    printer.vec3(camera.look_at_point);
+    std.debug.print("\n", .{});
+    printer.home();
+}
+
 pub fn main() !void {
-    glfw.setErrorCallback(errorCallback);
-    if (!glfw.init(.{})) {
-        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    }
-    defer glfw.terminate();
+    const wind = try window.Window.init(screen_width, screen_hight);
+    wind.hideCursor();
 
-    // Create our window
-    const window = glfw.Window.create(screen_width, screen_hight, "game-shit-dwag", null, null, .{
-        .opengl_profile = .opengl_core_profile,
-        .context_version_major = 4,
-        .context_version_minor = 5,
-    }) orelse {
-        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    };
-    defer window.destroy();
-    window.setInputModeCursor(.disabled);
-    defer window.setInputModeCursor(.normal);
-
-    glfw.makeContextCurrent(window);
-
-    const proc: glfw.GLProc = undefined;
-    try gl.load(proc, glGetProcAddress);
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    try GL_init(allocator);
+    try GL_init(allocator, wind);
 
-    // Wait for the user to close the window.
-
-    var anamation_thread = try std.Thread.spawn(
-        .{},
-        spin,
-        .{},
-    );
-    anamation_thread.detach();
+    // var anamation_thread = try std.Thread.spawn(.{}, spin, .{});
+    // anamation_thread.detach();
 
     var timer = try time.Timer.start();
-    while (!window.shouldClose()) : (i +%= 1) {
+    while (!wind.shouldClose()) : (i +%= 1) {
         const delta_time = @as(f32, @floatFromInt(timer.lap())) * 0.000000001;
+        const fps = @reduce(.Add, win);
+        std.debug.print("fps : {:.3}\r", .{fps});
         win[i] = (1 / (delta_time)) / win_size;
         glfw.pollEvents();
-        try input(&camera, delta_time, window);
-        // std.debug.print("angle = {}\n", .{angle});
-        // gl.clearColor(1, 0, 1, 1);
-        // gl.clear(gl.COLOR_BUFFER_BIT);
-        GL_Render();
-        window.swapBuffers();
+        const dir = try input(&camera, delta_time, wind);
+        GL_Render(dir);
+        wind.swapBuffer();
+        // time.sleep(10000000);
     }
     keep_running = true;
+    printer.clear();
+    obj_loader_service.deinit();
 }
 
-fn GL_init(allocator: Allocator) !void {
-    const light: vec.Vec4 = vec.init4(0, 5, 0, 1);
+fn GL_init(allocator: Allocator, wind: window.Window) !void {
+    prog = basic.BasicProgramTex.init();
 
-    prog = basic.BasicProgram.init();
-
-    const vert = try shader.Shader.init(allocator, "shaders/Seashell.vert", .vertex);
-    const frag = try shader.Shader.init(allocator, "shaders/Seashell.frag", .frag);
+    const text = try tex.Texture.init(allocator, "textures/Crab_D.tga");
+    _ = text;
+    const vert = try shader.Shader.init(allocator, "shaders/crab.vert", .vertex);
+    const frag = try shader.Shader.init(allocator, "shaders/crab.frag", .frag);
 
     prog.load_shader(vert);
     prog.load_shader(frag);
@@ -135,133 +114,118 @@ fn GL_init(allocator: Allocator) !void {
     prog.use();
 
     camera = cam.Camera.init(
-        40,
-        1,
-        1,
+        90,
+        wind.getAspectRatio(),
+        0.1,
         10000,
         vec.init3(0, 0, 0),
     );
 
-    const lighteye: vec.Vec4 = camera.view_matrix.MulVec(light);
-    prog.uniforms.lgtUniform.sendVec4(lighteye);
-
-    // gl.uniform4fv(lgtLoc, 1, @ptrCast(&lighteye.vec[0]));
-
+    mat.Mat4x4.debug_print_matrix(camera.projection_matrix);
+    const light: vec.Vec4 = vec.init4(5, 10, 7, 1);
+    // const lighteye: vec.Vec4 = camera.view_matrix.MulVec(light);
+    prog.uniforms.lgtUniform.sendVec4(light);
+    prog.uniforms.textureUniform.send1Uint(0);
     gl.clearColor(0, 0, 0, 1);
     gl.enable(gl.DEPTH_TEST); // cull face
     gl.cullFace(gl.BACK); // cull back face
 
-    var seaShell = render.renderer.init();
-    try seaShell.loadDatFile(allocator, "objects/Seashell.dat");
-    const basic_seaShell = obj.ObjectFactory.init(seaShell);
-    const rad = 2;
-
-    for (seaShells, 0..) |s, index| {
-        _ = s;
-        const normed = (@as(f32, @floatFromInt(index))) / 32.0 * 2.0 * std.math.pi;
-
-        seaShells[index] = basic_seaShell.make(
-            vec.init3(rad * @sin(normed), 0, rad * @cos(normed)),
-            vec.Vec3.zeros(),
+    obj_loader_service = try obj_loader.ObjectService.init(allocator);
+    crab = try obj_loader_service.load("objects/crab.obj", .obj);
+    const cube = try obj_loader_service.load("objects/cube.obj", .obj);
+    for (objects, 0..) |_, dex| {
+        objects[dex] = crab;
+        objects[dex].pos = vec.init3(
+            5 * @sin(@as(f32, @floatFromInt(dex))),
+            5 * @cos(@as(f32, @floatFromInt(dex))),
+            0,
         );
-        // std.debug.print("v : {}\n", .{seaShells[index].pos});
     }
+    objects[0] = crab;
+    objects[0].pos = vec.Vec3.number(0);
+    objects[0].roation = vec.Vec3.number(0);
 
-    seaShells[0].pos = vec.Vec3.number(1);
+    objects[1] = cube;
+    objects[1].pos = vec.Vec3.number(0);
+    objects[1].roation = vec.Vec3.number(0);
 
-    // for (seaShells, 0..) |s, index| {
-    //     std.debug.print("s[{}] : {}\n", .{ index, s });
-    // }
-    // gl.frontFace(gl.CW); // GL_CCW for counter clock-wise
-
+    objects[2] = cube;
+    objects[2].pos = vec.init3(light.x(), light.y(), light.z());
+    objects[2].roation = vec.Vec3.number(0);
 }
 
-fn GL_Render() void {
-    // const ident: mat.Mat4x4 = mat.Mat4x4.idenity()
-    // _ = ident;
-    // const rotationMatrix = ident.rotate(angle * CDR, vec.init3(0, 0, 1));
-    // const invMatrix = mvMatrix.t();
-    // const mvMatrix = viewMatrix.mul(rotationMatrix);
-    seaShells[0].updateRoation(vec.init3(angle * CDR, 0, 0));
+fn GL_Render(dir: vec.Vec3) void {
+    camera.updateFps(dir);
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    camera.update();
 
-    for (seaShells) |s| {
+    for (objects) |s| {
         prog.uniforms.draw(camera, s);
     }
 
-    prog.uniforms.draw(camera, seaShells[0]);
-
-    // gl.bindVertexArray(VAO);
-    // gl.drawElements(gl.TRIANGLES, @as(gl.GLsizei, @intCast(num_triangles)), gl.UNSIGNED_INT, null);
+    prog.uniforms.draw(camera, objects[0]);
 
     gl.flush();
 }
 
-fn clip(min_val: f32, max_val: f32, val: f32) f32 {
-    if (val >= min_val) return min_val;
-    if (val <= max_val) return max_val;
-    return val;
-}
-
 var last = vec.init2(0, 0);
+var pressed: bool = false;
+var speed: f32 = 3.0;
 
-fn input(came: *cam.Camera, delta_time: f32, window: glfw.Window) !void {
-    const speed = 2;
-    const radius = 10;
-    _ = radius;
-    const sense = 100;
-    const mouse_delta = window.getCursorPos();
+fn input(came: *cam.Camera, delta_time: f32, windor: window.Window) !vec.Vec3 {
+    const windr = windor.window;
+    const sense = 1000;
+    const mouse_delta = windr.getCursorPos();
     const mouse_vec = vec.init2(@floatCast(mouse_delta.xpos), @floatCast(mouse_delta.ypos));
     const delta = mouse_vec.vec - last.vec;
     last = mouse_vec;
 
+    var dir = vec.Vec3.zeros();
+
     came.yaw -= delta[0] * delta_time * sense;
     came.pitch -= (delta[1] * delta_time * sense);
-    came.pitch = clip(89, -89, came.pitch);
 
-    if (window.getKey(glfw.Key.w) == glfw.Action.press) {
-        // camera.eye.vec[0] += delta_time * speed;
-        camera.eye.vec[0] += delta_time * speed * @sin(came.yaw * CDR); //fowards
-        camera.eye.vec[2] += delta_time * speed * @cos(came.yaw * CDR); //right
-    }
-    // Move backward
-    if (window.getKey(glfw.Key.s) == glfw.Action.press) {
-        // camera.eye.vec[0] -= delta_time * speed;
-        camera.eye.vec[0] -= delta_time * speed * @sin(came.yaw * CDR);
-        camera.eye.vec[2] -= delta_time * speed * @cos(came.yaw * CDR);
-    }
+    if (windr.getKey(glfw.Key.w) == glfw.Action.press)
+        dir.set_x(speed * delta_time);
+    if (windr.getKey(glfw.Key.s) == glfw.Action.press)
+        dir.set_x(-speed * delta_time);
+    if (windr.getKey(glfw.Key.a) == glfw.Action.press)
+        dir.set_z(-speed * delta_time);
+    if (windr.getKey(glfw.Key.d) == glfw.Action.press)
+        dir.set_z(speed * delta_time);
+    if (windr.getKey(glfw.Key.space) == glfw.Action.press)
+        dir.set_y(speed * delta_time);
+    if (windr.getKey(glfw.Key.left_control) == glfw.Action.press)
+        dir.set_y(-speed * delta_time);
 
-    if (window.getKey(glfw.Key.a) == glfw.Action.press) {
-        // camera.eye.vec[0] += delta_time * speed;
-        camera.eye.vec[2] -= delta_time * speed * @cos((came.yaw - 90) * CDR);
-        camera.eye.vec[0] -= delta_time * speed * @sin((came.yaw - 90) * CDR);
-    }
-    // Move backward
-    if (window.getKey(glfw.Key.d) == glfw.Action.press) {
-        // camera.eye.vec[0] -= delta_time * speed;
-        camera.eye.vec[2] += delta_time * speed * @cos((came.yaw - 90) * CDR);
-        camera.eye.vec[0] += delta_time * speed * @sin((came.yaw - 90) * CDR);
-    }
+    if (windr.getKey(glfw.Key.minus) == glfw.Action.press)
+        speed += 1;
+    if (windr.getKey(glfw.Key.equal) == glfw.Action.press)
+        speed -= 1;
 
-    // Strafe right
-    if (window.getKey(glfw.Key.space) == glfw.Action.press) {
-        came.eye.vec[1] += delta_time * 10;
-        came.look_at_point.vec[1] += delta_time * 10;
+    if (windr.getKey(glfw.Key.f) == glfw.Action.press) {
+        index +%= 1;
+        if (index >= 512) index = 0;
+        objects[index] = crab;
+        objects[index].pos = camera.eye;
     }
 
-    if (window.getKey(glfw.Key.left_control) == glfw.Action.press) {
-        came.eye.vec[1] -= delta_time * 10;
-        came.look_at_point.vec[1] -= delta_time * 10;
+    if (windr.getKey(glfw.Key.r) == glfw.Action.press) {
+        if (prog.reload() == shader.ShaderErrors.failed_to_compile) {
+            std.debug.print("shader failed to complie\n", .{});
+        }
+    }
+    if (windr.getKey(glfw.Key.escape) == glfw.Action.press) {
+        windr.setShouldClose(true);
     }
 
-    if (window.getKey(glfw.Key.r) == glfw.Action.press) prog = try prog.reload();
+    const screnshot = windr.getKey(glfw.Key.p) == glfw.Action.press;
+    if (screnshot and !pressed) {
+        try windor.saveImg("screenshot.tga");
+        pressed = true;
+    } else if (!screnshot and pressed) {
+        pressed = false;
+    }
 
-    // came.look_at_point = vec.Vec3.number(1);
-
-    camera.look_at_point.vec[0] = camera.eye.vec[0] + (@sin(came.yaw * CDR));
-    camera.look_at_point.vec[1] = camera.eye.vec[1] + (@tan(came.pitch * CDR));
-    // camera.look_at_point.vec[1] = camera.eye.vec[1];
-    camera.look_at_point.vec[2] = camera.eye.vec[2] + (@cos(came.yaw * CDR));
+    return dir;
 }
