8fc54ac5346e9b47a0331574356f58201bda1365
sirlilpanda <sirlilpanda@gmail.com>
2024-06-24 18:06:14 +1200

    struct to hold the texture

---
+++ src/opengl_wrappers/texture.zig
@@ -0,0 +1,58 @@
+const gl = @import("gl");
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const file = @import("../file_loading/tga.zig");
+
+var count: c_uint = 0;
+
+pub const Texture = struct {
+    const Self = @This();
+    texture_id: gl.GLuint,
+    texture_spot: gl.GLenum,
+
+    pub fn init(allocator: Allocator, filename: []const u8) !Self {
+        var self = Self{
+            .texture_id = undefined,
+            .texture_spot = undefined,
+        };
+
+        const data = try file.Tga.load(allocator, filename);
+        defer allocator.free(data.data);
+        gl.genTextures(1, &self.texture_id);
+        gl.activeTexture(gl.TEXTURE0);
+        count += 1;
+        self.texture_spot = count;
+        gl.bindTexture(gl.TEXTURE_2D, self.texture_id);
+        const fomat: gl.GLenum = switch (data.header.bits_per_pixel) {
+            1 * 8 => gl.R8,
+            3 * 8 => gl.BGR,
+            4 * 8 => gl.RGBA,
+            else => gl.R8,
+        };
+        // const int_format: gl.GLint = switch (data.header.bits_per_pixel) {
+        //     1 * 8 => 1,
+        //     3 * 8 => 3,
+        //     4 * 8 => 4,
+        //     else => 1,
+        // };
+
+        gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,
+            gl.RGB,
+            data.header.wdith,
+            data.header.height,
+            0,
+            fomat,
+            gl.UNSIGNED_BYTE,
+            @ptrCast(&data.data[0]),
+        );
+
+        gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+        return self;
+    }
+};
