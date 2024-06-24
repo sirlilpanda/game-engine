eb70f150c722a0def90f996be8cb9bfbadbc1f7e
sirlilpanda <sirlilpanda@gmail.com>
2024-06-24 18:01:44 +1200

    nenow correctly unloads the object

--- src/file_loading/loadfile.zig
+++ src/file_loading/loadfile.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const builtin = @import("builtin");
 const Allocator = std.mem.Allocator;
 const mem = std.mem;
+const Colour = @import("../console_logger/coloured_text.zig").Colour;
+
 pub usingnamespace @import("load_data_file.zig");
+pub usingnamespace @import("load_obj_file.zig");
 
 pub const eol: []const u8 = if (builtin.os.tag == .windows) "\r\n" else "\n";
 
@@ -19,7 +22,7 @@ pub const ObjectFile = struct {
     texture: []f32,
     allocator: Allocator,
 
-    pub fn change_orign(self: Self, x: f32, y: f32, z: f32) void {
+    pub fn change_origin(self: Self, x: f32, y: f32, z: f32) void {
         var i: usize = 0;
         while (i < self.verts.len) : (i += 3) {
             self.verts[i] += x;
@@ -30,8 +33,13 @@ pub const ObjectFile = struct {
     }
 
     pub fn unload(file: Self) void {
+        // std.debug.print("elements : len {}\n", .{file.elements.len});
+        // std.debug.print("verts : len {}\n", .{file.verts.len});
+        // std.debug.print("normals : len {}\n", .{file.normals.len});
+        // std.debug.print("texture : len {}\n", .{file.texture.len});
         file.allocator.free(file.elements);
         file.allocator.free(file.verts);
         file.allocator.free(file.normals);
+        file.allocator.free(file.texture);
     }
 };
