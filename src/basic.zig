2acdbd654af780cc96085a9f89c079cde9bf2eb7
sirlilpanda <sirlilpanda@gmail.com>
2024-06-24 17:57:40 +1200

    added new basic program that has textures

--- src/basic.zig
+++ src/basic.zig
@@ -16,18 +16,53 @@ pub const BasicUniforms = struct {
     pub fn draw(self: Self, camera: cam.Camera, object: obj.Object) void {
         // i dont know if this is faster
         var model = mat.Mat4x4.idenity();
-        model = model.mul(mat.Mat4x4.translate(object.pos)).mul(mat.Mat4x4.rotate(object.roation.vec[0], vec.init3(1, 0, 0)));
+
+        model = model.mul(mat.Mat4x4.translate(object.pos)).mul(mat.Mat4x4.rotate(object.roation.vec[1], vec.init3(1, 1, 0)));
 
         const mvMatrix = camera.view_matrix.mul(model);
         const mvpMatrix = camera.projection_matrix.mul(mvMatrix);
-        // const invMatrix = mvMatrix.inverseTranspose();
         const invMatrix = mvMatrix.inverseTranspose();
+
         //could queue in another thread to speed up times
         self.mvMatrixUniform.sendMatrix4(false, mvMatrix);
         self.mvpMatrixUniform.sendMatrix4(false, mvpMatrix);
         self.norMatrixUniform.sendMatrix4(false, invMatrix);
+        // self.lgtUniform.sendVec4(vec.init4(camera.eye.vec[0], camera.eye.vec[1], camera.eye.vec[2], 0));
         object.draw();
     }
 };
 
 pub const BasicProgram = program.Program(BasicUniforms);
+
+pub const BasicUniformsText = struct {
+    const Self = @This();
+    mMatrixUniform: uni.Uniform = uni.Uniform.init("mMatrix"),
+    mvMatrixUniform: uni.Uniform = uni.Uniform.init("mvMatrix"),
+    mvpMatrixUniform: uni.Uniform = uni.Uniform.init("mvpMatrix"),
+    norMatrixUniform: uni.Uniform = uni.Uniform.init("norMatrix"),
+    lgtUniform: uni.Uniform = uni.Uniform.init("lightPos"),
+    textureUniform: uni.Uniform = uni.Uniform.init("tSampler"),
+
+    pub fn draw(self: Self, camera: cam.Camera, object: obj.Object) void {
+        // i dont know if this is faster
+
+        var model = mat.Mat4x4.idenity();
+
+        model = model.mul(mat.Mat4x4.translate(object.pos))
+            .mul(mat.Mat4x4.rotate(object.roation.vec[1], vec.init3(1, 1, 0)));
+        self.mMatrixUniform.sendMatrix4(false, model);
+
+        const mvMatrix = camera.view_matrix.mul(model);
+        const mvpMatrix = camera.projection_matrix.mul(mvMatrix);
+        const invMatrix = mvMatrix.inverseTranspose();
+
+        //could queue in another thread to speed up times
+        self.mvMatrixUniform.sendMatrix4(false, mvMatrix);
+        self.mvpMatrixUniform.sendMatrix4(false, mvpMatrix);
+        self.norMatrixUniform.sendMatrix4(false, invMatrix);
+        // self.lgtUniform.sendVec4(vec.init4(camera.eye.vec[0], camera.eye.vec[1], camera.eye.vec[2], 0));
+        object.draw();
+    }
+};
+
+pub const BasicProgramTex = program.Program(BasicUniformsText);
