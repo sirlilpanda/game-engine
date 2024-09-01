acce2b17d6d27655f3a132921868a45555dea4f3
sirlilpanda <sirlilpanda@gmail.com>
2024-09-01 18:12:44 +1200

    now works depending on what programs are avaible

--- src/app.zig
+++ src/app.zig
@@ -25,134 +25,165 @@ const prog_error = error{
     program_not_in_provided_programs,
 };
 
-pub const App = struct {
-    const Self = @This();
-    window: Window,
-    physic_objects: std.ArrayList(*obj.Object),
-    obj_loader_service: obj_loader.ObjectService,
-    camera: cam.Camera,
-    alloc: Allocator, // just incase you want a global alloc
-    delta_time: f32,
-    timer: time.Timer,
+pub fn App(comptime Programs: type) type {
+    return struct {
+        const Self = @This();
+        window: Window,
+        programs: Programs,
+        physic_objects: std.ArrayList(*obj.Object),
+        obj_loader_service: obj_loader.ObjectService,
+        camera: cam.Camera,
+        alloc: Allocator, // just incase you want a global alloc
+        delta_time: f32,
+        timer: time.Timer,
 
-    pub fn init(screen_width: u32, screen_hight: u32, alloc: Allocator) !Self {
-        var self = Self{
-            .window = undefined,
-            .physic_objects = undefined,
-            .obj_loader_service = try obj_loader.ObjectService.init(alloc),
-            .camera = undefined,
-            .alloc = alloc,
-            .delta_time = 0,
-            .timer = try time.Timer.start(),
-        };
+        // [TODO] work out how to write this
+        // fn check_prog_struct(self: Self) void {
+        //     inline for (std.meta.fields(Programs)) |f| {
+        //     }
+        // }
 
-        self.window = try Window.init(screen_width, screen_hight);
-        self.camera = cam.Camera.init(
-            90,
-            self.window.getAspectRatio(),
-            0.1,
-            10000,
-            vec.init3(0, 0, 0),
-        );
+        pub fn init(screen_width: u32, screen_hight: u32, alloc: Allocator, programs: Programs) !Self {
+            var self = Self{
+                .window = undefined,
+                .programs = programs,
+                .physic_objects = undefined,
+                .obj_loader_service = try obj_loader.ObjectService.init(alloc),
+                .camera = undefined,
+                .alloc = alloc,
+                .delta_time = 0,
+                .timer = try time.Timer.start(),
+            };
 
-        gl.clearColor(0, 0, 0, 1);
-        gl.enable(gl.DEPTH_TEST); // cull face
-        gl.cullFace(gl.BACK); // cull back face
+            self.window = try Window.init(screen_width, screen_hight);
+            self.camera = cam.Camera.init(
+                90,
+                self.window.getAspectRatio(),
+                0.1,
+                10000,
+                vec.init3(0, 0, 0),
+            );
 
-        return self;
-    }
+            gl.clearColor(0, 0, 0, 1);
+            gl.enable(gl.DEPTH_TEST); // cull face
+            gl.cullFace(gl.BACK); // cull back face
 
-    pub fn shouldStop(self: *Self) bool {
-        self.delta_time = @as(f32, @floatFromInt(self.timer.lap())) * 0.000000001;
-        return self.window.shouldClose();
-    }
+            self.window.hideCursor();
 
-    pub fn input(self: *Self) void {
-        const sense = 1000;
-        const delta = self.window.getCursorDelta();
-        var speed: f32 = 100;
-        var dir = vec.Vec3.zeros();
-        self.camera.yaw -= delta.x() * self.delta_time * sense;
-        self.camera.pitch -= (delta.y() * self.delta_time * sense);
-
-        if (self.window.window.getKey(glfw.Key.w) == glfw.Action.press) {
-            std.debug.print("w : pressed", .{});
-            dir.set_x(speed * self.delta_time);
-        }
-        if (self.window.window.getKey(glfw.Key.s) == glfw.Action.press) {
-            std.debug.print("s : pressed", .{});
-            dir.set_x(-speed * self.delta_time);
-        }
-        if (self.window.window.getKey(glfw.Key.a) == glfw.Action.press) {
-            std.debug.print("a : pressed", .{});
-            dir.set_z(-speed * self.delta_time);
-        }
-        if (self.window.window.getKey(glfw.Key.d) == glfw.Action.press) {
-            std.debug.print("d : pressed", .{});
-            dir.set_z(speed * self.delta_time);
-        }
-        if (self.window.window.getKey(glfw.Key.space) == glfw.Action.press) {
-            std.debug.print("space : pressed", .{});
-            dir.set_y(speed * self.delta_time);
-        }
-        if (self.window.window.getKey(glfw.Key.left_control) == glfw.Action.press) {
-            std.debug.print("left_control : pressed", .{});
-            dir.set_y(-speed * self.delta_time);
+            return self;
         }
 
-        if (self.window.window.getKey(glfw.Key.escape) == glfw.Action.press) {
-            self.window.window.setShouldClose(true);
+        pub fn shouldStop(self: *Self) bool {
+            self.delta_time = @as(f32, @floatFromInt(self.timer.lap())) * 0.000000001;
+            return self.window.shouldClose();
         }
 
-        self.camera.updateFps(dir);
-    }
-    // current program
-    // camera
-    // render_thread
-    // physics_thread
-    // loader
-    // alloc
+        // program name must be what you called it with the struct you passed into the app
+        pub fn change_program(self: Self, program_name: []const u8) !void {
+            inline for (std.meta.fields(Programs)) |f| {
+                if (wrapper.trait_check(f.type, "use")) {
+                    @field(self.programs, program_name).use();
+                } else {
+                    return prog_error.program_not_in_provided_programs;
+                }
+            }
+        }
 
-    pub fn free(self: *Self) void {
-        self.window.deinit();
-        self.obj_loader_service.deinit();
-    }
+        pub fn render(self: Self) void {
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+            inline for (std.meta.fields(Programs)) |f| {
+                @field(self.programs, f.name).use();
+                @field(self.programs, f.name).renderAll();
+            }
+            self.window.swapBuffer();
+
+            gl.flush();
+        }
+
+        pub fn input(self: *Self) void {
+            std.debug.print("delta time {}\n", .{self.delta_time});
+            glfw.pollEvents();
+            const sense = 10.0;
+            const delta = self.window.getCursorDelta();
+            var speed: f32 = 10;
+            var dir = vec.Vec3.zeros();
+            self.camera.yaw -= delta.x() * self.delta_time * sense;
+            self.camera.pitch -= (delta.y() * self.delta_time * sense);
+
+            if (self.window.window.getKey(glfw.Key.w) == glfw.Action.press) {
+                std.debug.print("w : pressed\n", .{});
+                dir.set_x(speed * self.delta_time);
+            }
+            if (self.window.window.getKey(glfw.Key.s) == glfw.Action.press) {
+                std.debug.print("s : pressed\n", .{});
+                dir.set_x(-speed * self.delta_time);
+            }
+            if (self.window.window.getKey(glfw.Key.a) == glfw.Action.press) {
+                std.debug.print("a : pressed\n", .{});
+                dir.set_z(-speed * self.delta_time);
+            }
+            if (self.window.window.getKey(glfw.Key.d) == glfw.Action.press) {
+                std.debug.print("d : pressed\n", .{});
+                dir.set_z(speed * self.delta_time);
+            }
+            if (self.window.window.getKey(glfw.Key.space) == glfw.Action.press) {
+                std.debug.print("space : pressed\n", .{});
+                dir.set_y(speed * self.delta_time);
+            }
+            if (self.window.window.getKey(glfw.Key.left_control) == glfw.Action.press) {
+                std.debug.print("left_control : pressed\n", .{});
+                dir.set_y(-speed * self.delta_time);
+            }
+            // std.debug.print("dir", .{});
+            // if (self.window.getKey(glfw.Key.minus) == glfw.Action.press)
+            //     speed += 1;
+            // if (self.window.getKey(glfw.Key.equal) == glfw.Action.press)
+            //     speed -= 1;
+
+            // if (self.window.getKey(glfw.Key.r) == glfw.Action.press) {
+            //     if (self.prog.reload() == shader.ShaderErrors.failed_to_compile) {
+            //         std.debug.print("shader failed to complie\n", .{});
+            //     }
+            // }
+
+            if (self.window.window.getKey(glfw.Key.escape) == glfw.Action.press) {
+                self.window.window.setShouldClose(true);
+            }
+
+            self.camera.updateFps(dir);
+
+            // if (self.window.getKey(glfw.Key.p) == glfw.Action.up) ;
+            // if (screnshot and !pressed) {
+            //     try windor.saveImg("screenshot.tga");
+            //     pressed = true;
+            // } else if (!screnshot and pressed) {
+            //     pressed = false;
+            // }
+
+            // return dir;
+        }
+
+        // current program
+        // camera
+        // render_thread
+        // physics_thread
+        // loader
+        // alloc
+
+        pub fn free(self: *Self) void {
+            self.window.deinit();
+            inline for (std.meta.fields(Programs)) |f| {
+                @field(self.programs, f.name).unload();
+            }
+            self.obj_loader_service.deinit();
+        }
+    };
+}
+
+pub const BasicPrograms = struct {
+    basic_program_texture: basic.BasicProgramTex,
+    // basic_program: basic.BasicProgram,
 };
 
-// const App = @import("app.zig");
-// const basic = @import("basic.zig");
-// const std = @import("std");
-// const shader = @import("/opengl_wrappers/shader.zig");
-// const tex = @import("opengl_wrappers/texture.zig");
-// const gl = @import("gl");
-// const glfw = @import("mach-glfw");
-
-// pub fn main() !void {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer _ = gpa.deinit();
-//     const allocator = gpa.allocator();
-
-//     var app = try App.App.init(
-//         1920,
-//         1080,
-//         allocator,
-//     );
-
-//     var basic_program_texture = try basic.createBasicProgramWTexture(allocator);
-//     basic_program_texture.camera = &app.camera;
-
-//     basic_program_texture.objects[0] = try app.obj_loader_service.load("objects/Crab.obj", .obj);
-//     _ = try tex.Texture.init(allocator, "textures/Crab_D.tga");
-
-//     std.debug.print("obj : {any}\n", .{basic_program_texture.objects[0]});
-
-//     while (!app.shouldStop()) {
-//         app.input();
-
-//         basic_program_texture.renderAll();
-
-//         std.time.sleep(10000000);
-//     }
-
-//     app.free();
-// }
+pub const BasicApp = App(BasicPrograms);
