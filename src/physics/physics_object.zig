8ecb922a80fdaa11171b01d21cbaac950c71157e
sirlilpanda <sirlilpanda@gmail.com>
2024-09-08 14:09:21 +1200

    wrapper for an object to apply physics to them

--- src/physics/physics_object.zig
+++ src/physics/physics_object.zig
@@ -1,14 +1,23 @@
 const Object = @import("../objects/object.zig").Object;
-const HitBox = @import("hit_box.zig");
+
+const vec = @import("../math/vec.zig");
+const Collider = @import("collision.zig").Collider;
 
 pub const PhysicsObject = struct {
     const Self = @This();
     obj: *Object,
-    hitbox: HitBox,
+    velocity: vec.Vec3,
+    force: vec.Vec3,
+    mass: f32,
+    collider: Collider,
 
-    pub fn init(obj: *Object) Self {
+    pub fn init(obj: *Object, mass: f32) Self {
         return PhysicsObject{
             .obj = obj,
+            .velocity = vec.Vec3.zeros(),
+            .force = vec.Vec3.zeros(),
+            .mass = mass,
+            .collider = undefined,
         };
     }
 };
