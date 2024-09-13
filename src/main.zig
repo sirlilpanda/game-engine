13b405c009f83aa81f14742acc423da1d1d90fb9
sirlilpanda <sirlilpanda@gmail.com>
2024-09-13 22:13:12 +1200

    added doc comments

--- src/main.zig
+++ src/main.zig
@@ -19,13 +19,13 @@ pub fn main() !void {
         undefined,
     );
 
+    // has to be done this way to ensure that opengl is init'ed
     app.programs.basic_program_texture = try basic.createBasicProgramWTexture(allocator);
     app.programs.basic_program_texture.camera = &app.camera;
 
     app.programs.basic_program_texture.objects[0] = try app.obj_loader_service.load("objects/Crab.obj", .obj);
     var plane = try app.obj_loader_service.load("objects/plane.obj", .obj);
     plane.texture = tex.Texture.init(allocator, "textures/sky_box_.tga") catch null;
-    std.debug.print("plane\n", .{});
     plane.pos = vec.init3(10.0, 2.0, 4.0);
 
     var cube = try app.obj_loader_service.load("objects/cube.obj", .obj);
@@ -35,13 +35,18 @@ pub fn main() !void {
     var crab = try app.obj_loader_service.load("objects/Crab.obj", .obj);
     crab.texture = try tex.Texture.init(allocator, "textures/Crab_D.tga");
 
-    var sphere = try app.obj_loader_service.load("objects/sphere.obj", .obj);
+    var sphere = try app.obj_loader_service.load("objects/donut.obj", .obj);
     sphere.texture = try tex.Texture.init(allocator, "textures/Earth.bmp");
     for (app.programs.basic_program_texture.objects, 0..) |_, dex| {
         var ject: obj.Object = undefined;
 
         ject = crab;
         ject.pos = vec.init3(
+            2 * @sin(@as(f32, @floatFromInt(dex))),
+            0,
+            2 * @cos(@as(f32, @floatFromInt(dex))),
+        );
+        ject.roation = vec.init3(
             2 * @sin(@as(f32, @floatFromInt(dex))),
             2 * @cos(@as(f32, @floatFromInt(dex))),
             0,
@@ -54,10 +59,12 @@ pub fn main() !void {
     app.programs.basic_program_texture.objects[0].?.colour = vec.init4(2, 2, 2, 1);
 
     app.programs.basic_program_texture.objects[1] = sphere;
+    app.programs.basic_program_texture.objects[1].?.pos = vec.init3(0, -0.2, 0);
+
     app.programs.basic_program_texture.objects[1].?.scale = vec.init3(1, 1, 1);
     app.programs.basic_program_texture.objects[1].?.colour = vec.init4(1, 0, 1, 1);
     // app.programs.basic_program_texture.objects[1] = try app.obj_loader_service.load("objects/4V5T.obj", .obj);
-    // app.programs.basic_program_texture.objects[1].?.texture = tex.Texture.init(allocator, "textures/rgb.tga") catch null;
+    // app.programs.basic_program_texture.objects[1].?=.texture = tex.Texture.init(allocator, "textures/rgb.tga") catch null;
     // app.programs.basic_program_texture.objects[1].?.colour = vec.init4(0.7, 0.7, 0.7, 1);
 
     std.debug.print("obj : {any}\n", .{app.programs.basic_program_texture.objects[0]});
@@ -76,15 +83,25 @@ pub fn main() !void {
         b += 0.00013 * b_dir;
         g += 0.0007 * g_dir;
     }) {
+        // gets the input
         app.input();
+        // renders all the objects
         app.render();
 
-        // for (app.programs.basic_program_texture.objects, 0..) |_, dex| {
-        //     if (dex != 0) {
-        //         app.programs.basic_program_texture.objects[dex].?.updateRoation(vec.init3(angle + 90, angle + 180, angle + 23));
-        //         app.programs.basic_program_texture.objects[dex].?.updateColour(vec.init4(r, b, g, 1));
-        //     }
-        // }
+        var dex: usize = 2;
+        while (dex < app.programs.basic_program_texture.objects.len) : (dex += 1) {
+            app.programs.basic_program_texture.objects[dex].?.pos = vec.init3(
+                2 * @sin(@as(f32, @floatFromInt(dex)) + angle),
+                0,
+                2 * @cos(@as(f32, @floatFromInt(dex)) + angle),
+            );
+            app.programs.basic_program_texture.objects[dex].?.roation = vec.init3(
+                2 * @sin(@as(f32, @floatFromInt(dex)) + angle),
+                2 * @cos(@as(f32, @floatFromInt(dex)) + angle),
+                0,
+            );
+        }
+
         std.debug.print("fps : {}]\r", .{app.fps()});
         // std.debug.print("cam {}\n", app.camera.eye);
         if (r > 1) r_dir = -1;
@@ -95,5 +112,6 @@ pub fn main() !void {
         if (g < 0) g_dir = 1;
     }
 
+    // frees the app
     app.free();
 }
