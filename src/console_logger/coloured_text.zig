0adffc899cd430548c2a1bce8bebd4894093e179
sirlilpanda <sirlilpanda@gmail.com>
2024-09-14 00:08:08 +1200

    now has a fmt function and can be used to colour text in places

--- src/console_logger/coloured_text.zig
+++ src/console_logger/coloured_text.zig
@@ -9,27 +9,38 @@ pub const ColourPrinter = struct {
     /// this is the colour of the text
     colour_fg: Colour,
 
+    colour_set_string: [38:0]u8 = colour_set_string_fmt.*,
+    colour_end_string: [4:0]u8 = colour_end_string_fmt.*,
+
+    const colour_set_string_fmt = "\x1B[38;2;{d};{d};{d}m\x1B[48;2;{d};{d};{d}m";
+    const colour_end_string_fmt = "\x1B[0m";
+
     pub fn init() Self {
-        return Self{
+        var self = Self{
             .colour_fg = Colour.white(),
             .colour_bg = Colour.black(),
         };
+
+        self.updateColourSetString();
+        return self;
     }
 
     /// sets the forground/text colour
     pub fn setFgColour(self: *Self, colour: Colour) void {
         self.colour_fg = colour;
+        self.updateColourSetString();
     }
 
     /// sets background colour
     pub fn setBgColour(self: *Self, colour: Colour) void {
         self.colour_bg = colour;
+        self.updateColourSetString();
     }
 
-    /// prints a given string with the set colours
-    pub fn print(self: Self, comptime format: []const u8, args: anytype) void {
-        std.debug.print(
-            "\x1B[38;2;{d};{d};{d}m\x1B[48;2;{d};{d};{d}m",
+    fn updateColourSetString(self: *Self) void {
+        _ = std.fmt.bufPrint(
+            &self.colour_set_string,
+            colour_set_string_fmt,
             .{
                 self.colour_fg.r,
                 self.colour_fg.g,
@@ -38,9 +49,24 @@ pub const ColourPrinter = struct {
                 self.colour_bg.g,
                 self.colour_bg.b,
             },
-        );
-        std.debug.print(format, args);
-        std.debug.print("\x1B[0m", .{});
+        ) catch |err| std.debug.print("couldnt update colour set string error {any}\n", .{err});
+
+        // std.debug.print("buf = {s}\n", .{self.colour_set_string});
+    }
+
+    // /// prints a given string with the set colours
+    // pub fn print(self: Self, comptime format: []const u8, args: anytype) void {
+    //     std.debug.print(self.colour_set_string);
+    //     std.debug.print(format, args);
+    //     std.debug.print(self.colour_end_string, .{});
+    // }
+
+    pub fn getColourSetString(self: Self) [50:0]u8 {
+        return self.colour_set_string;
+    }
+
+    pub fn getColourEndString(self: Self) [50:0]u8 {
+        return self.colour_end_string;
     }
 
     /// returns the cursor back to the home / 0, 0 pos on the terminal
@@ -76,5 +102,39 @@ pub const ColourPrinter = struct {
         std.debug.print("}}", .{});
 
         self.setFgColour(old_fg);
+    }
+
+    /// idea fmt = {f:red|b:blue} will set the forground and back ground colour
+    /// i will implement this later, current fmt start will print the start string
+    /// and fmt end will print the end string
+    pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = options;
+        if (fmt.len > 0) {
+            if (std.mem.eql(u8, fmt, "start")) {
+                try writer.print(
+                    colour_set_string_fmt,
+                    .{
+                        self.colour_fg.r,
+                        self.colour_fg.g,
+                        self.colour_fg.b,
+                        self.colour_bg.r,
+                        self.colour_bg.g,
+                        self.colour_bg.b,
+                    },
+                );
+            } else if (std.mem.eql(u8, fmt, "end")) {
+                try writer.print(colour_end_string_fmt, .{});
+            } else {
+                try writer.print(
+                    "{any}",
+                    .{self},
+                );
+            }
+        } else {
+            try writer.print(
+                "{any}",
+                .{self},
+            );
+        }
     }
 };
