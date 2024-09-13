912b5da6a67653849c2273afbfb4a61be069a2e9
sirlilpanda <sirlilpanda@gmail.com>
2024-09-14 00:07:11 +1200

    added a few more colours, usize to colour function and a random colour function

--- src/utils/colour.zig
+++ src/utils/colour.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 /// colour type
 pub const Colour = struct {
     const Self = @This();
@@ -26,6 +28,11 @@ pub const Colour = struct {
         return Self{ .r = 255, .g = 255, .b = 0 };
     }
 
+    /// inits the struct with a gold colour
+    pub fn gold() Self {
+        return Self{ .r = 255, .g = 200, .b = 0 };
+    }
+
     /// inits the struct with a lightGreen colour
     pub fn lightGreen() Self {
         return Self{ .r = 128, .g = 255, .b = 0 };
@@ -39,6 +46,11 @@ pub const Colour = struct {
     /// inits the struct with a cyan colour
     pub fn cyan() Self {
         return Self{ .r = 0, .g = 255, .b = 128 };
+    }
+
+    /// inits the struct with a turquoise colour
+    pub fn turquoise() Self {
+        return Self{ .r = 0, .g = 255, .b = 190 };
     }
 
     /// inits the struct with a lightBlue colour
@@ -61,14 +73,24 @@ pub const Colour = struct {
         return Self{ .r = 170, .g = 0, .b = 255 };
     }
 
-    /// inits the struct with a pink colour
-    pub fn pink() Self {
-        return Self{ .r = 128, .g = 0, .b = 255 };
+    /// inits the struct with a lightPurple colour
+    pub fn lightPurple() Self {
+        return Self{ .r = 196, .g = 78, .b = 255 };
     }
 
     /// inits the struct with a magenta colour
     pub fn magenta() Self {
         return Self{ .r = 255, .g = 0, .b = 255 };
+    }
+
+    /// inits the struct with a pink colour
+    pub fn pink() Self {
+        return Self{ .r = 255, .g = 0, .b = 128 };
+    }
+
+    /// inits the struct with a peach colour
+    pub fn peach() Self {
+        return Self{ .r = 255, .g = 158, .b = 158 };
     }
 
     /// inits the struct with a black colour
@@ -90,5 +112,41 @@ pub const Colour = struct {
     pub fn greyScale(scale: u8) Self {
         // 255 max
         return Self{ .r = scale, .g = scale, .b = scale };
+    }
+
+    /// this will return a set colour based on the usize number
+    /// how ever at at point it will start to be random
+    pub fn usizeToColour(number: usize) Self {
+        return switch (number) {
+            0 => red(),
+            1 => orange(),
+            2 => yellow(),
+            3 => gold(),
+            4 => lightGreen(),
+            5 => green(),
+            6 => cyan(),
+            7 => turquoise(),
+            8 => lightBlue(),
+            9 => blue(),
+            10 => darkPurple(),
+            11 => purple(),
+            12 => lightPurple(),
+            13 => magenta(),
+            14 => pink(),
+            15 => peach(),
+            else => random(), // just make a random colour
+        };
+    }
+
+    pub fn random() Self {
+        const static = struct {
+            var prng = std.rand.DefaultPrng.init(69420);
+        };
+        const rand = static.prng.random();
+        return custom(
+            rand.int(u8),
+            rand.int(u8),
+            rand.int(u8),
+        );
     }
 };
