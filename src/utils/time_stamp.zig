6a85a6e93ce2569b7d9ca1f7faa38c48052178c8
sirlilpanda <sirlilpanda@gmail.com>
2024-09-14 12:21:13 +1200

    basic epoch time stamp wrapper for converting epochs to a more human readable format

---
+++ src/utils/time_stamp.zig
@@ -0,0 +1,111 @@
+const time = @import("std").time;
+const std = @import("std");
+
+const TimeStamp = struct {
+    const Self = @This();
+    millisec: u64,
+    sec: u64,
+    min: u64,
+    hour: u64,
+    day: u64,
+    month: u64,
+    year: u64,
+
+    pub fn current() Self {
+        const current_time: u64 = @abs(time.timestamp());
+        return fromTime(current_time);
+    }
+
+    fn lengthOfMonth(month: u64, year: u64) u64 {
+        return switch (month) {
+            1 => 2678400,
+            3 => 2678400,
+            5 => 2678400,
+            7 => 2678400,
+            8 => 2678400,
+            10 => 2678400,
+            12 => 2678400,
+            4 => 2592000,
+            6 => 2592000,
+            9 => 2592000,
+            11 => 2592000,
+            else => blk: {
+                if (!isLeapYear(year)) {
+                    break :blk 2505600;
+                } else {
+                    break :blk 2419200;
+                }
+            },
+        };
+    }
+
+    fn isLeapYear(year: u64) bool {
+        return (year % 100 == 0 and year % 400 == 0) or (year % 100 != 0 and year % 4 == 0);
+    }
+
+    fn lengthOfYear(year: u64) u64 {
+        if (isLeapYear(year)) {
+            return 31622400;
+        } else {
+            return 31536000;
+        }
+    }
+
+    pub fn fromTime(time_stamp: u64) Self {
+        var temp_time_stamp: u64 = time_stamp;
+        var year: u64 = 1970;
+
+        while (temp_time_stamp >= lengthOfYear(year)) {
+            temp_time_stamp -= lengthOfYear(year);
+            year += 1;
+        }
+
+        var month: u64 = 1;
+
+        while (temp_time_stamp >= lengthOfMonth(month, year)) {
+            temp_time_stamp -= lengthOfMonth(month, year);
+            month += 1;
+        }
+
+        const day: u64 = temp_time_stamp / 86400 + 1;
+        temp_time_stamp %= 86400;
+
+        const hour: u64 = temp_time_stamp / 3600;
+        temp_time_stamp %= 3600;
+
+        const min: u64 = temp_time_stamp / 60;
+        temp_time_stamp %= 60;
+
+        const sec: u64 = temp_time_stamp;
+
+        return Self{
+            .millisec = 0,
+            .sec = sec,
+            .min = min,
+            .hour = hour + 12,
+            .day = day,
+            .month = month,
+            .year = year,
+        };
+    }
+
+    pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+        try writer.print("{d:.4}-{d:.2}-{d:.2}_{d:.2}:{d:.2}:{d:.2}", .{
+            self.day,
+            self.month,
+            self.year,
+            self.hour,
+            self.min,
+            self.sec,
+        });
+    }
+};
+
+test "works" {
+    const time_stamp = TimeStamp.current();
+    // const time_stamp = TimeStamp.fromTime(0);
+
+    std.debug.print("time : {}\n", .{time_stamp});
+}
