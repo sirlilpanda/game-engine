5d183921ccffb92fd8e36a510fbda384059553eb
sirlilpanda <sirlilpanda@gmail.com>
2024-09-15 20:19:04 +1200

    added logging and changed format checking to be within image instead of texture

--- src/textures/texture.zig
+++ src/textures/texture.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Image = @import("../file_loading/image.zig").Image;
 
+const texture_logger = std.log.scoped(.Texture);
+
 /// texture struct, where the texture is store in the gpu
 /// only supports 2d textures, still needs a free
 pub const Texture = struct {
@@ -14,24 +16,21 @@ pub const Texture = struct {
 
     /// creates a new texture form the file
     pub fn init(allocator: Allocator, filename: []const u8) !Self {
+        texture_logger.debug("creating texture form file {s}", .{filename});
         var self = Self{
             .texture_id = undefined,
             .texture_spot = undefined,
         };
-        const image = try Image.init(allocator, filename);
 
-        defer allocator.free(image.data);
+        const image = try Image.init(allocator, filename);
+        defer image.unload(allocator);
         gl.genTextures(1, &self.texture_id);
         gl.activeTexture(gl.TEXTURE0);
         self.texture_spot = gl.TEXTURE0;
         gl.bindTexture(gl.TEXTURE_2D, self.texture_id);
+        texture_logger.debug("generated texture {s} with id {}", .{ filename, self.texture_id });
 
-        const format: gl.GLenum = switch (image.bits_per_pixel) {
-            1 * 8 => gl.R8,
-            3 * 8 => gl.RGB,
-            4 * 8 => gl.RGBA,
-            else => gl.R8,
-        };
+        texture_logger.debug("texture {s} has format {s}", .{ filename, @tagName(image.format) });
 
         gl.texImage2D(
             gl.TEXTURE_2D,
@@ -40,10 +39,12 @@ pub const Texture = struct {
             image.width,
             image.height,
             0,
-            format,
+            @intFromEnum(image.format),
             gl.UNSIGNED_BYTE,
             @ptrCast(&image.data[0]),
         );
+
+        texture_logger.debug("texture {s} sent to gpu", .{filename});
 
         gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
@@ -61,7 +62,7 @@ pub const Texture = struct {
 
     /// frees the texture from the GPU
     pub fn destroy(self: Self) void {
-        std.debug.print("[INFO] deleting texture with id {}\n", .{self.texture_id});
+        texture_logger.info("[INFO] deleting texture with id {}", .{self.texture_id});
         gl.deleteTextures(1, self.texture_id);
     }
 };
