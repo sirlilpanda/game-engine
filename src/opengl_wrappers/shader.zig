e04f6c23a23f24bfb1381bb6943862d6c32788ce
sirlilpanda <sirlilpanda@gmail.com>
2024-09-15 20:20:08 +1200

    added logging

--- src/opengl_wrappers/shader.zig
+++ src/opengl_wrappers/shader.zig
@@ -3,13 +3,15 @@ const std = @import("std");
 
 const Allocator = std.mem.Allocator;
 
+const shader_logger = std.log.scoped(.Shader);
+
 /// loads the current file with the given allocator
 pub fn loadfile(allocator: Allocator, filename: []const u8) ![]u8 {
     const file = std.fs.cwd().openFile(filename, .{});
     var buffer: [256]u8 = undefined;
 
     if (file == std.fs.File.OpenError.FileNotFound) {
-        std.debug.print("file : {s} not found\n", .{try std.fs.cwd().realpath(filename, &buffer)});
+        shader_logger.err("file : {s} not found", .{try std.fs.cwd().realpath(filename, &buffer)});
     }
 
     const shader_file: std.fs.File = try file;
@@ -17,36 +19,15 @@ pub fn loadfile(allocator: Allocator, filename: []const u8) ![]u8 {
     const file_end = try shader_file.getEndPos();
     const data = try allocator.alloc(u8, @as(usize, file_end + 1));
     const data_read = try shader_file.readAll(data);
-    _ = data_read;
-    // std.debug.print("read : {}\n", .{data_read});
-    // std.debug.print("len : {}\n", .{data.len});
+    shader_logger.debug("read {} bytes", .{data_read});
+    // std.debug.print("read : {}", .{data_read});
+    // std.debug.print("len : {}", .{data.len});
     data[file_end] = 0;
     return data;
 }
 
-/// [depracted] dont use this anymore create a program using the program struct
-pub fn createShaderProgram(vertex_shader_path: []const u8, fragment_shader_path: []const u8) !gl.GLuint {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const vertex: Shader = try Shader.init(allocator, vertex_shader_path, gl.VERTEX_SHADER);
-    const frag: Shader = try Shader.init(allocator, fragment_shader_path, gl.FRAGMENT_SHADER);
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vertex.id);
-    gl.attachShader(program, frag.id);
-    gl.linkProgram(program);
-    gl.useProgram(program);
-
-    return program;
-}
-
 /// just a defualt name for if the shader is static
 pub const static_shader_name = "static";
-
-/// i really need to remove this
-const quiet: bool = true;
 
 /// shaders errors
 pub const ShaderErrors = error{
@@ -78,14 +59,13 @@ pub const Shader = struct {
 
     /// creates a new shader from the given shader path
     pub fn init(allocator: Allocator, shader_path: []const u8, shader_type: ShaderTypes) !Self {
+        shader_logger.info("loading shader {s}, as {s}", .{ shader_path, @tagName(shader_type) });
         const shaderfile: []u8 = loadfile(allocator, shader_path) catch |err| {
-            std.debug.print("[ERROR] failed to load shader file {s}, got error {any}", .{ shader_path, err });
+            shader_logger.err("failed to load shader file {s}, got error {any}", .{ shader_path, err });
             return err;
         };
 
         defer allocator.free(shaderfile);
-
-        if (!quiet) std.debug.print("\nfile : {s}\n", .{shaderfile});
 
         const shader_id = gl.createShader(switch (shader_type) {
             ShaderTypes.vertex => gl.VERTEX_SHADER,
@@ -111,7 +91,7 @@ pub const Shader = struct {
 
     /// creates a new shader, but doesnt load in the file, instead accpets an loaded file
     pub fn initWithFile(allocator: Allocator, shader_file: []const u8, shader_type: ShaderTypes) !Self {
-        if (!quiet) std.debug.print("\nfile : {s}\n", .{shader_file});
+        shader_logger.info("loading shader static, as {s}", .{@tagName(shader_type)});
 
         const shader_id = gl.createShader(switch (shader_type) {
             ShaderTypes.vertex => gl.VERTEX_SHADER,
@@ -139,31 +119,33 @@ pub const Shader = struct {
     fn checkStatus(self: Self, allocator: Allocator) !void {
         var status: gl.GLint = undefined;
         gl.getShaderiv(self.id, gl.COMPILE_STATUS, &status);
+        shader_logger.info("checking shader {s} status ", .{self.shader_path});
         if (status == gl.FALSE) {
             var info_log_len: gl.GLsizei = undefined;
             gl.getShaderiv(self.id, gl.INFO_LOG_LENGTH, &info_log_len);
             const error_string: []u8 = try allocator.alloc(u8, @as(usize, @intCast(info_log_len)));
             defer allocator.free(error_string);
             gl.getShaderInfoLog(self.id, info_log_len, null, @ptrCast(&error_string[0]));
-            std.debug.print("[ERROR] shader {s} got error when compling : {s}\n", .{ self.shader_path, error_string });
+            shader_logger.err("shader {s} got error when compling : {s}", .{ self.shader_path, error_string });
             return ShaderErrors.failed_to_compile;
         }
+        shader_logger.info("shader {s} complied successfully", .{self.shader_path});
     }
 
     /// calls when the shader file is reloaded
     pub fn reload(self: Self) !Self {
         if (std.mem.eql(u8, self.shader_path, static_shader_name)) {
-            std.debug.print("[ERROR] attempted to reload static shader\n", .{});
+            shader_logger.err("attempted to reload static shader", .{});
             return ShaderErrors.static_shader_cant_reload;
         } else {
-            std.debug.print("[INFO] reloading shader {s}\n", .{self.shader_path});
+            shader_logger.info("reloading shader {s}", .{self.shader_path});
             return try init(self.allocator, self.shader_path, self.shader_type);
         }
     }
 
     /// unloads the shader
     pub fn unload(self: Self) void {
-        std.debug.print("[INFO] unloading shader {s}\n", .{self.shader_path});
+        shader_logger.info("unloading shader {s}", .{self.shader_path});
         gl.deleteShader(self.id);
     }
 };
