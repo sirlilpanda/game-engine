07de666bf33b8263212f100de13414063ac7072e
sirlilpanda <sirlilpanda@gmail.com>
2024-11-23 11:51:34 +1300

    changed from using multiple renderers to one render type using the new vbo and vao abstractions

--- src/opengl_wrappers/render.zig
+++ src/opengl_wrappers/render.zig
@@ -1,33 +1,134 @@
 const gl = @import("gl");
 const file = @import("../file_loading/loadfile.zig");
 const std = @import("std");
+const VAO = @import("vao.zig").VAO;
+const VBO = @import("vbo.zig").VBO;
+const DrawType = @import("gl_types.zig").DrawType;
+
+const logger_renderer = std.log.scoped(.Renderer);
 
 const Allocator = std.mem.Allocator;
 
-const RenderType = enum {
-    render_3d,
-    render_2d,
-};
-
-pub const Renderer = union(RenderType) {
+pub const Renderer = struct {
     const Self = @This();
-    render_3d: Render3d,
-    render_2d: Render2d,
+
+    vertex_array_object: VAO,
+    vertex_buffer_objects: []VBO,
+    number_elements: usize,
+    alloc: Allocator,
+
+    pub fn init(allocator: Allocator, buffers: []VBO) Self {
+        var self = Self{
+            .vertex_array_object = VAO.init(@intCast(buffers.len)),
+            .number_elements = 0,
+            .vertex_buffer_objects = buffers,
+            .alloc = allocator,
+        };
+        logger_renderer.info("creating new renderer with id {} and {} buffers", .{ self.vertex_array_object.id, buffers.len });
+        self.vertex_array_object.bindVAO();
+        for (buffers) |vbo| {
+            self.vertex_array_object.bindVBO(@constCast(&vbo));
+            if (vbo.target == gl.ELEMENT_ARRAY_BUFFER) {
+                self.number_elements = vbo.buffer_size;
+            }
+        }
+
+        return self;
+    }
 
     pub fn destroy(self: Self) void {
-        switch (self) {
-            .render_3d => self.destroy(),
-            .render_2d => self.destroy(),
+        logger_renderer.info("deleting vao with id {}", .{
+            self.vertex_array_object.id,
+        });
+
+        for (self.vertex_buffer_objects) |vbo| {
+            vbo.delete();
         }
+        self.alloc.free(self.vertex_buffer_objects);
+        self.vertex_array_object.delete();
+    }
+
+    pub fn drawElementsTriangles(self: Self) void {
+        self.vertex_array_object.bindVAO();
+        gl.drawElements(
+            gl.TRIANGLES,
+            @as(gl.GLsizei, @intCast(self.number_elements)),
+            gl.UNSIGNED_INT,
+            null,
+        );
+    }
+
+    pub fn drawElementsPatches(self: Self) void {
+        self.vertex_array_object.bindVAO();
+        gl.drawElements(
+            gl.PATCHES,
+            @as(gl.GLsizei, @intCast(self.number_elements)),
+            gl.UNSIGNED_INT,
+            null,
+        );
+    }
+
+    pub fn drawInstanced(self: Self) void {
+        self.vertex_array_object.bindVAO();
+        gl.drawArraysInstanced(
+            gl.TRIANGLE_STRIP,
+            0,
+            @as(gl.GLsizei, @intCast(self.number_elements)),
+            1,
+        );
     }
 };
 
-const logger_render2d = std.log.scoped(.Render_2d);
+pub fn objectRenderer(allocator: Allocator, dat: file.ObjectFile) !Renderer {
+    const buffers = allocator.alloc(VBO, 4) catch |err| {
+        logger_renderer.err("could not allocate vbos in objectRenderer got error {any}", .{err});
+        return err;
+    };
+    buffers[0] = VBO.init(
+        "vertex_buffer_object",
+        gl.ARRAY_BUFFER,
+        .static,
+        3,
+        .float,
+        @ptrCast(&dat.verts[0]),
+        dat.verts.len,
+    );
+    buffers[1] = VBO.init(
+        "vertex_normal_object",
+        gl.ARRAY_BUFFER,
+        .static,
+        3,
+        .float,
+        @ptrCast(&dat.normals[0]),
+        dat.normals.len,
+    );
+    buffers[2] = VBO.init(
+        "vertex_texture_object",
+        gl.ARRAY_BUFFER,
+        .static,
+        2,
+        .float,
+        @ptrCast(&dat.texture[0]),
+        dat.texture.len,
+    );
+    buffers[3] = VBO.init(
+        "vertex_index_object",
+        gl.ELEMENT_ARRAY_BUFFER,
+        .static,
+        undefined,
+        undefined,
+        @ptrCast(&dat.elements[0]),
+        dat.elements.len,
+    );
+    logger_renderer.info("created new render with {} verts", .{dat.verts.len / 3});
+    return Renderer.init(allocator, buffers);
+}
 
-const logger_render3d = std.log.scoped(.Render_3d);
-
-pub const Render2d = struct {
-    const Self = @This();
+pub fn quad2dRenderer(allocator: Allocator) !Renderer {
+    const buffer = allocator.alloc(VBO, 1) catch |err| {
+        logger_renderer.err("could not allocate vbos in quad2dRenderer got error {any}", .{err});
+        return err;
+    };
 
     const quad_verts = [8]f32{
         0.0, 1.0,
@@ -36,220 +137,19 @@ pub const Render2d = struct {
         1.0, 0.0,
     };
 
-    vertex_array_object: gl.GLuint,
+    buffer[0] = VBO.init( // index 0
+        "vertex_buffer_object",
+        gl.ARRAY_BUFFER,
+        .static,
+        2,
+        .float,
+        @ptrCast(&quad_verts[0]),
+        quad_verts.len,
+    );
 
-    vertex_buffer_object: gl.GLuint,
+    var ren = Renderer.init(allocator, buffer);
 
-    /// creates a new render with buffer loactions on the gpu
-    pub fn init() Self {
-        var self = Self{
-            .vertex_array_object = undefined,
-            .vertex_buffer_object = undefined,
-        };
+    ren.number_elements = quad_verts.len;
 
-        gl.genVertexArrays(1, &self.vertex_array_object);
-        gl.genBuffers(1, &self.vertex_buffer_object);
-
-        logger_render2d.info("creating 2d renderer with id {}", .{self.vertex_array_object});
-        gl.bindVertexArray(self.vertex_array_object);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_buffer_object);
-        gl.namedBufferData(
-            self.vertex_buffer_object,
-            @as(isize, @intCast(quad_verts.len * @sizeOf(gl.GLfloat))),
-            @ptrCast(&quad_verts[0]),
-            gl.STATIC_DRAW,
-        );
-        gl.enableVertexArrayAttrib(self.vertex_array_object, 0);
-        gl.vertexAttribPointer(0, 2, gl.FLOAT, gl.FALSE, 0, null);
-
-        return self;
-    }
-
-    pub fn render(self: Self) void {
-        gl.bindVertexArray(self.vertex_array_object);
-        gl.drawArraysInstanced(
-            gl.TRIANGLE_STRIP,
-            0,
-            @as(gl.GLsizei, @intCast(quad_verts.len)),
-            1,
-        );
-    }
-};
-
-/// this store all the buffer data locations for
-/// 3d objects, i will be changing this to a union
-/// in the future to allow for differnt buffers
-/// arrangements to be used
-pub const Render3d = struct {
-    const Self = @This();
-    /// the id of the array object
-    vertex_array_object: gl.GLuint,
-    /// the vertex data
-    vertex_buffer_object: gl.GLuint,
-    /// the vertex normal data
-    vertex_normal_object: gl.GLuint, //might remove later
-    /// this vertex texture data
-    vertex_texture_object: gl.GLuint,
-    /// the face index data
-    vertex_index_object: gl.GLuint,
-    /// the number of elements, i.e. the length of the arrays
-    number_elements: usize,
-
-    /// creates a new render with buffer loactions on the gpu
-    pub fn init() Self {
-        var self = Self{
-            .vertex_array_object = undefined,
-            .vertex_buffer_object = undefined,
-            .vertex_normal_object = undefined, //might remove later
-            .vertex_texture_object = undefined,
-            .vertex_index_object = undefined,
-            .number_elements = undefined,
-        };
-        logger_render3d.info("creating new 3d renderer", .{});
-
-        gl.genVertexArrays(1, &self.vertex_array_object);
-        // could make this faster if i gen
-        gl.genBuffers(1, &self.vertex_buffer_object);
-        gl.genBuffers(1, &self.vertex_normal_object);
-        gl.genBuffers(1, &self.vertex_index_object);
-        gl.genBuffers(1, &self.vertex_texture_object);
-
-        logger_render3d.info("created 3d renderer with id {}", .{self.vertex_array_object});
-        // gl.bindVertexArray(self.vertex_array_object);
-        // gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_buffer_object);
-        // gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_normal_object);
-        // gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_index_object);
-        // gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_texture_object);
-
-        return self;
-    }
-
-    /// loads a new objectFile in to the gpu
-    pub fn loadFile(self: *Self, dat: file.ObjectFile) !void {
-        logger_render3d.info("loading object file in to the gpu", .{});
-        gl.bindVertexArray(self.vertex_array_object);
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_buffer_object);
-        gl.namedBufferData(
-            self.vertex_buffer_object,
-            @as(isize, @intCast(dat.verts.len * @sizeOf(gl.GLfloat))),
-            @ptrCast(&dat.verts[0]),
-            gl.STATIC_DRAW,
-        );
-        gl.enableVertexArrayAttrib(self.vertex_array_object, 0);
-        gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 0, null);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_normal_object);
-        gl.namedBufferData(
-            self.vertex_normal_object,
-            @as(isize, @intCast(dat.normals.len * @sizeOf(f32))),
-            @ptrCast(&dat.normals[0]),
-            gl.STATIC_DRAW,
-        );
-        gl.enableVertexArrayAttrib(self.vertex_array_object, 1);
-        gl.vertexAttribPointer(1, 3, gl.FLOAT, gl.FALSE, 0, null);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, self.vertex_texture_object);
-        gl.namedBufferData(
-            self.vertex_texture_object,
-            @as(isize, @intCast(dat.texture.len * @sizeOf(f32))),
-            @ptrCast(&dat.texture[0]),
-            gl.STATIC_DRAW,
-        );
-
-        gl.enableVertexArrayAttrib(self.vertex_array_object, 2);
-        gl.vertexAttribPointer(2, 2, gl.FLOAT, gl.FALSE, 0, null);
-
-        // std.debug.print("text : {any}", .{dat.texture.len / 2});
-        // std.debug.print("vert : {any}", .{dat.verts.len / 3});
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.vertex_index_object);
-        gl.namedBufferData(
-            self.vertex_index_object,
-            @as(isize, @intCast(dat.elements.len * @sizeOf(u32))),
-            @ptrCast(&dat.elements[0]),
-            gl.STATIC_DRAW,
-        );
-
-        logger_render3d.info("sent data to the gpu", .{});
-        self.number_elements = dat.elements.len;
-
-        logger_render3d.info("unloading object data", .{});
-        logger_render3d.debug("this unload may not occur in the future", .{});
-        dat.unload();
-
-        gl.bindVertexArray(self.vertex_array_object);
-    }
-
-    /// destroies all the buffers on the gpu
-    pub fn destroy(self: Self) void {
-        logger_render3d.info("deleting 3d renderer with id {}", .{self.vertex_array_object});
-        gl.bindVertexArray(self.vertex_array_object);
-
-        gl.deleteVertexArrays(1, &self.vertex_array_object);
-        gl.deleteBuffers(1, &self.vertex_buffer_object);
-        gl.deleteBuffers(1, &self.vertex_normal_object);
-        gl.deleteBuffers(1, &self.vertex_index_object);
-        gl.deleteBuffers(1, &self.vertex_texture_object);
-    }
-
-    /// renders the object
-    pub fn render(self: Self) void {
-        gl.bindVertexArray(self.vertex_array_object);
-        gl.drawElements(
-            gl.TRIANGLES,
-            @as(gl.GLsizei, @intCast(self.number_elements)),
-            gl.UNSIGNED_INT,
-            null,
-        );
-    }
-};
-
-// fn loadSeaShell() !void {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer _ = gpa.deinit();
-//     const allocator = gpa.allocator();
-
-//     const dat = try file_reader.DatFile.loadDatFile(allocator, "objects/Seashell.dat");
-
-//     gl.genVertexArrays(1, &VAO);
-//     gl.bindVertexArray(VAO);
-
-//     var VBOids: [3]gl.GLuint = undefined;
-
-//     gl.genBuffers(3, &VBOids);
-//     // std.debug.print("VBO {} {} {}", .{ VBOids[0], VBOids[1], VBOids[2] });
-
-//     gl.bindBuffer(gl.ARRAY_BUFFER, VBOids[0]);
-//     gl.bufferData(
-//         gl.ARRAY_BUFFER,
-//         @as(isize, @intCast(dat.verts.len * @sizeOf(gl.GLfloat))),
-//         @ptrCast(&dat.verts[0]),
-//         gl.STATIC_DRAW,
-//     );
-//     gl.enableVertexAttribArray(0);
-//     gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 0, null);
-
-//     gl.bindBuffer(gl.ARRAY_BUFFER, VBOids[1]);
-//     gl.bufferData(
-//         gl.ARRAY_BUFFER,
-//         @as(isize, @intCast(dat.normals.len * @sizeOf(f32))),
-//         @ptrCast(&dat.normals[0]),
-//         gl.STATIC_DRAW,
-//     );
-//     gl.enableVertexAttribArray(1);
-//     gl.vertexAttribPointer(1, 3, gl.FLOAT, gl.FALSE, 0, null);
-
-//     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, VBOids[2]);
-//     gl.bufferData(
-//         gl.ELEMENT_ARRAY_BUFFER,
-//         @as(isize, @intCast(dat.elements.len * @sizeOf(u32))),
-//         @ptrCast(&dat.elements[0]),
-//         gl.STATIC_DRAW,
-//     );
-
-//     num_triangles = dat.elements.len;
-
-//     dat.unload();
-
-//     gl.bindVertexArray(0);
-// }
+    return ren;
+}
